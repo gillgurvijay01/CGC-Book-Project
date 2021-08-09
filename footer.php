aa931f9a05173eee98f23c864ff5aa1727778698
Gurvijay <gillgurvijay01@gmail.com>
2021-08-09 11:52:34 +0530

    Header and footer built

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?PHP
+echo '
+<footer class="bg-dark footer-dark" style="position: fixed;
+height: 50px;
+bottom: 0px;
+left: 0px;
+right: 0px;
+margin-bottom: 0px;">
+
+  <div class="footer-dark text-center  py-3">
+  <span style="color:white">Made by the team | CGC Book Project | 2021 
+  </div>
+
+</footer>
+';
+?>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">CGC Book Project</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sign Up     <i class="fas fa-user-plus"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login       <i class="fas fa-sign-in-alt"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Admin  Login     <i class="fas fa-users-cog"></i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+
+<?php
+include "footer.php";
+?>
+</body>
+</html>
