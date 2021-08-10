abcfb9cd4c47495f023cbb6e9ae6a6c6a4bbae04
Gurvijay <gillgurvijay01@gmail.com>
2021-08-10 12:56:28 +0530

    admin index building - pages to be designed
    admin session needs to be build

--- admin-entry.php
+++ admin-entry.php
@@ -1,9 +1,23 @@
 <?php
 $con=mysqli_connect( "localhost",  "gurvijay" , "password" , "bookProject") or
 die(mysqli_error($con));
-$email=$_POST['email'];
+$uname=$_POST['username'];
 $password=$_POST['password'];
+if (($uname == 'admin') && ($password == 'password')){
+    $timestamp = date("Y-m-d H:i:s ");
+    $admin_track_query="INSERT INTO authorise ( user , clock_time ) VALUES ('$uname','$timestamp')";
+    $submit_track = mysqli_query($con,$admin_track_query)
+    or die(mysqli_error($con));
+    header('Location:admin-index.php');}
+else    {
+    echo "You are not authorized user <br>";
+    echo  "Try Again";
+    $timestamp = date("Y-m-d H:i:s ");
+    $unauthorize="INSERT INTO unauthorized_admin_track ( user , password , time ) VALUES ('$uname','$password','$timestamp')";
+    $submit=mysqli_query($con,$unauthorize)
+    or die(mysqli_error($con));
 
+}
 
 
 ?>
---
+++ admin-header.php
@@ -0,0 +1,28 @@
+<?php
+   echo '<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+   <a class="navbar-brand" href="index.php">Admin Panel</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarNav">
+     <ul class="navbar-nav ml-auto">
+       <li class="nav-item">
+         <a class="nav-link" href="admin-all-users.php">All Users     <i class="fas fa-users"></i></a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="admin-all-products.php">Products      <i class="fas fa-luggage-cart"></i></a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="admin-user-products.php">User Products Ordered     <i class="fas fa-shopping-cart"></i></a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="admin-admin-unauth.php">Admin Unauth Entry <i class="fas fa-exclamation-triangle"></i></a>
+       </li>
+       <li class="nav-item">
+       <a class="nav-link" href="admin-log-out.php">Log Out  <i class="fas fa-sign-out-alt"></i></a>
+     </li>
+       
+     </ul>
+   </div>
+ </nav>'
+?>
